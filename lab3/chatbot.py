a590f21a8efe02f749c7c24ddf0eaff5c9276e8e
Renwei <24462780@life.hkbu.edu.hk>
2025-02-28 16:48:26 +0800

    [Lab3]

--- lab3/chatbot.py
+++ lab3/chatbot.py
@@ -1,33 +1,35 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import configparser
 import logging
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def main():
+    # Load your token and create an Updater for your Bot
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    token = config['TELEGRAM']['ACCESS_TOKEN']
+    
+    # Create the Updater
+    updater = Updater(token=token, use_context=True)
+    dispatcher = updater.dispatcher
+
+    # Set up logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    # Register a dispatcher to handle messages
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+
+    # To start the bot:
+    updater.start_polling()
+    updater.idle()
+
+def echo(update: Update, context: CallbackContext) -> None:
     reply_message = update.message.text.upper()
     logging.info("Update: %s", update)
     logging.info("Context: %s", context)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
-
-def main():
-    # Load the configuration file
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-
-    # Setup logging
-    logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-    )
-
-    # Create the Application
-    application = ApplicationBuilder().token(config['TELEGRAM']['ACCESS_TOKEN']).build()
-
-    # Add message handler
-    echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    application.add_handler(echo_handler)
-
-    # Start the Bot
-    application.run_polling()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
 
 if __name__ == '__main__':
     main()
